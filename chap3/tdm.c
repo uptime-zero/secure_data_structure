cf190ff1ec1391a532b1f0ee1d0fb20dae3d50e0
uptime-zero <dereckjun2173@gmail.com>
2026-03-31 01:52:06 +0900

    chap3: TDM 축소하기 최적화
    - 희소 행렬을 사용하기 위해 구조체 선언
    - 값을 실시간으로 읽으면서 0이 아닌 값만 구조체 배열에 저장 & 유효 데이터 개수 증가
    - 희소 행렬을 순서대로 출력

--- chap3/tdm.c
+++ chap3/tdm.c
@@ -11,6 +11,12 @@
 #include <stdio.h>
 #include <windows.h>
 
+typedef struct {
+    int row;
+    int col;
+    int val;
+} Element;
+
 int main() {
     // 입출력 시 한글 깨짐 방지
     SetConsoleOutputCP(65001);
@@ -19,7 +25,8 @@ int main() {
     // 1. 변수 선언
     int N, M;  // 단어(N) 및 문서 개수(M)
     char terms[31][21];  // 단어 개수와 단어의 길이
-    int matrix[31][51];  // 전체 크기 (단어 개수 * 문서 개수)
+    Element sparse_matrix[1500];  // 전체 크기
+    int count = 0;  // 실제로 저장된 원소 개수
 
     // 2. 단어 개수, 단어, 문서 개수 입력
     scanf("%d", &N);
@@ -29,20 +36,29 @@ int main() {
 
     scanf("%d", &M);
 
+    // 3. TDM을 읽으면서 0이 아닌 것만 바로 희소 행렬에 저장
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
-            scanf("%d", &matrix[i][j]);
+            int input_value;
+            scanf("%d", &input_value);
+
+            if (input_value != 0) {
+                sparse_matrix[count].row = i;
+                sparse_matrix[count].col = j;
+                sparse_matrix[count].val = input_value;
+                count++; // 저장된 원소 개수 증가
+            }
+            // 0이면 저장 안 함
         }
     }
 
-    // 3. 0이 아닌 값만 출력
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            if (matrix[i][j] == 0) {
-                continue;
-            }
-            printf("%s 문서%d %d\n", terms[i], j + 1, matrix[i][j]);
-        }
+    // 4. 희소 행렬을 순서대로 출력
+    for (int i = 0; i < count; i++) {
+        printf("%s 문서%d %d\n",
+            terms[sparse_matrix[i].row],   // 행 번호 → 단어
+            sparse_matrix[i].col + 1,      // 열 번호 → 문서번호 (1부터 시작)
+            sparse_matrix[i].val
+        );
     }
 
     return 0;
